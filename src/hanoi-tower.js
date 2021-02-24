69033509c08374b13e6ce74115489481a220b92e
milothea <sauchanka.liubou@gmail.com>
2021-02-24 12:26:01 +0300

    feat: implement a hanoi tower_s algorythm

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const steps = 2**disksNumber - 1;
+
+  return {turns: steps, seconds: Math.floor(steps / (turnsSpeed / 60**2))};
 };
