748e47a47c354e6245df2c91373c08c457ab786a
milothea <sauchanka.liubou@gmail.com>
2021-02-24 12:22:27 +0300

    feat: implement function to creat a dream team name using input

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !(Array.isArray(members)) ) {
+    return false;
+  } else if (members === []) {
+    return null;
+  }
+
+  let dreamTeamName = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') {
+      continue;
+    }
+    let member;
+
+    if (members[i].indexOf(' ') != -1 ) {
+      member = members[i].replace(/^\s+/g, '').split(' ');
+    } else {
+      member = members[i].split(' ');
+    }
+
+    if (member.length > members[i]) {
+      member.forEach (item => dreamTeamName.push(item[0].toUpperCase()));
+    } else {
+      dreamTeamName.push(member[0][0].toUpperCase());
+    }
+  }
+
+  return dreamTeamName.sort().join('');
 };
