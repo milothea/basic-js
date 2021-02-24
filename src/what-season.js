a484ba8e6822c574a414a4f7e6d0cbaceccb87e5
milothea <sauchanka.liubou@gmail.com>
2021-02-24 12:28:14 +0300

    feat: implement a function to output season following an input of date

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else if (!(date instanceof Date)) {
+    throw new Error ('Invalid date format! Use "new Date" to create valid date.');
+  }
+  try {
+    date.valueOf();
+  } catch (e) {
+    throw new Error ('Invalid date format! Use "new Date" to create valid date.');
+  }
+
+  const month = date.getMonth();
+
+  if (2 <= month && month <=4 ) {
+    return 'spring';
+  } else if (5 <= month && month <= 7) {
+    return 'summer';
+  } else if (8 <= month && month <= 10) {
+    return 'autumn';
+  } else {
+    return 'winter';
+  }
 };
