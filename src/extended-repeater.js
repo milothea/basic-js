5acc8c1a19381089d57b9cc700609c8a6ceb38f5
milothea <sauchanka.liubou@gmail.com>
2021-03-24 18:09:38 +0300

    feat: implement function to repeat strings under condition in options

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  console.log(str, options);
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let newStr = str,
+      {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options;
+
+  if (additionRepeatTimes || addition) {
+    newStr += transformStr(addition, additionSeparator, additionRepeatTimes, true);
+  }
+
+  return transformStr(newStr, separator, repeatTimes);
+
+  function transformStr(string, separator, repeats, add = false) {
+    let sep = '',
+        repTimes = (repeats) ? repeats : 1;
+        transformedStr = string,
+        cycleCounter = (repTimes * 2) - 1;
+
+    if (add) {
+      sep = (separator) ? separator : '|';
+    } else {
+      sep = (separator) ? separator : '+';
+    }
+
+    for (let i = 1; i < cycleCounter; i++) {
+      if (i % 2 != 0) {
+        transformedStr += sep;
+      } else {
+        transformedStr += string;
+      }
+    }
+
+    return transformedStr;
+  }
 };
   
