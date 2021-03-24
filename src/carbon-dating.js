e4f3f7ce8a6d9d4f66666b880910e80e398f1ad9
milothea <sauchanka.liubou@gmail.com>
2021-03-24 18:55:42 +0300

    feat: implement function to calculate an age of sample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const sample = +sampleActivity;
+
+  if (typeof sampleActivity != 'string' ||
+      isNaN(sample) ||
+      sample <= 0 ||
+      sample > MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY / sample)) / (0.693 / HALF_LIFE_PERIOD));
 };
